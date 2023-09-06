f6482118a11587f9e2e40102a5cec1ae9be8c6d4
iremberber <iremberber0@gmail.com>
2023-09-06 23:11:12 +0300

    Kodları Tamamladım
    Bu web sitesinin amacı, müzik dinlemek isteyen kullanıcılara eğlenceli ve görsel bir deneyim sunmaktır. Site, Taylor Swift'in bazı şarkılarını seçenek olarak sunar ve her şarkı için albüm kapakları ve dönen plak görselleri gibi görsel efektlerle kullanıcıları etkilemeye çalışır. Ayrıca, kullanıcıların herhangi bir şarkıyı seçtiklerinde ilgili albüm kapakları ve plakları animasyonlarla gösterir, bu da müziği daha görsel bir deneyim haline getirir.

--- muzik.js
+++ muzik.js
@@ -1,35 +1,82 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var loverAudio = document.getElementById('lover');
-    var speaknowAudio = document.getElementById('speaknow');
-    var fearlessAudio = document.getElementById('fearless');
-    var albumDiv = document.getElementById('album');
-    
-    var audioElements = [loverAudio, speaknowAudio, fearlessAudio];
-    
-    audioElements.forEach(function (audio) {
-        audio.addEventListener('play', function () {
-            albumDiv.style.backgroundImage = getBackgroundImageForAudio(audio.id);
-            
-            // Diğer audio elementlerini durdurun
-            audioElements.forEach(function (otherAudio) {
-                if (otherAudio !== audio && !otherAudio.paused) {
-                    otherAudio.pause();
-                }
-            });
+    const laudioElement = document.getElementById("lover");
+    const saudioElement = document.getElementById("speaknow");
+    const faudioElement = document.getElementById("fearless");
+    const albumLover = document.getElementById("albumlover");
+    const albumSpeakNow = document.getElementById('albumspeaknow');
+    const albumFearless = document.getElementById('albumfearless');
+    const plakLover = document.getElementById('plaklover');
+    const plakSpeakNow = document.getElementById('plakspeaknow');
+    const plakFearless = document.getElementById('plakfearless');
+
+    function stopOtherAudios(currentAudioElement) {
+        const audioElements = [laudioElement, saudioElement, faudioElement];
+
+        audioElements.forEach((audioElement) => {
+            if (audioElement !== currentAudioElement && !audioElement.paused) {
+                audioElement.pause();
+            }
         });
-        
-        audio.addEventListener('pause', function () {
-            albumDiv.style.backgroundImage = "none";
-        });
+    }
+
+    function playAudio(audioElement, albumElement, plakElement) {
+
+        setTimeout(function () {
+        albumElement.style.bottom = "150px";
+        plakElement.style.bottom = "150px";
+        }, 3000);
+
+        setTimeout(function () {
+            albumElement.style.left = "150px";
+            plakElement.style.left = "150px";
+        }, 4000);
+
+        setTimeout(function () {
+            plakElement.style.opacity = "1";
+            plakElement.style.left = "300px";
+        }, 5000);
+
+        stopOtherAudios(audioElement);
+    }
+
+    laudioElement.addEventListener("play", function () {
+        playAudio(laudioElement, albumLover, plakLover);
     });
 
-    function getBackgroundImageForAudio(audioId) {
-        const backgrounds = {
-            'lover': "url(https://upload.wikimedia.org/wikipedia/tr/c/cd/Taylor_Swift_-_Lover.png)",
-            'speaknow': "url(https://i.scdn.co/image/ab67616d0000b2730b04da4f224b51ff86e0a481)",
-            'fearless': "url(https://hips.hearstapps.com/hmg-prod/images/148022559-884819482089040-4676896142298036016-n-1613146852.jpg?crop=0.850xw:0.850xh;0.129xw,0.111xh&resize=1200:*)",
-        };
+    saudioElement.addEventListener("play", function () {
+        playAudio(saudioElement, albumSpeakNow, plakSpeakNow);
+    });
 
-        return backgrounds[audioId] || "none";
+    faudioElement.addEventListener("play", function () {
+        playAudio(faudioElement, albumFearless, plakFearless);
+    });
+
+    function pauseAudio(audioElement, albumElement, plakElement) {
+        
+        plakElement.style.left = "150px";
+        albumElement.style.left = "150px";
+
+        setTimeout(function () {
+            plakElement.style.left = "";
+            plakElement.style.opacity = "";
+            albumElement.style.left = "";
+        }, 1000);
+
+        setTimeout(function () {
+            albumElement.style.bottom = "";
+            plakElement.style.bottom = "";
+        }, 2000);
     }
+
+    laudioElement.addEventListener("pause", function () {
+        pauseAudio(laudioElement, albumLover, plakLover);
+    });
+
+    saudioElement.addEventListener("pause", function () {
+        pauseAudio(saudioElement, albumSpeakNow, plakSpeakNow);
+    });
+
+    faudioElement.addEventListener("pause", function () {
+        pauseAudio(faudioElement, albumFearless, plakFearless);
+    });
 });
